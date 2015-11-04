be06682b7285a027bd86b5101dceb84e35614e52
nishant-jain-94 <nishantkumarajain@gmail.com>
2015-11-04 16:29:26 +0530

    made changes to padding and anchor tags.

--- js/fetch.js
+++ js/fetch.js
@@ -5,7 +5,7 @@ $.ajax({
           data.forEach(function(datum) {
           var name = datum.Name;
           var username = datum.candidate.split('@')[0];
-          $("#candidates").append('<li><a href="http://ec2-54-251-140-208.ap-southeast-1.compute.amazonaws.com/~'+username+'"'+'>'+name+'</a></li>');
+          $("#candidates").append('<li><a href="http://ec2-54-251-140-208.ap-southeast-1.compute.amazonaws.com/~'+username+'"'+' target="_blank">'+name+'</a></li>');
         });
       }
     });
